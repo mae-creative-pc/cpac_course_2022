70de2094c1ce0471896837c43fc4284b1c38d450
rickykubler <82145414+rickykubler@users.noreply.github.com>
2022-10-02 16:51:18 +0200

    lab1_ex3

--- labs/lab1_tool_world/exercises/ex3_human_readable/your_code.py
+++ labs/lab1_tool_world/exercises/ex3_human_readable/your_code.py
@@ -1,5 +1,5 @@
 import numpy as np
-TEACHER_CODE=True
+TEACHER_CODE=False
 def sort_songs(audio_features):
     """"Receive audio features and sort them according to your criterion"
 
@@ -17,7 +17,15 @@ def sort_songs(audio_features):
         for idx in random_idxs:
             sorted_songs.append(audio_features[idx])
     else:
-        pass
         # your code here
+        denceability = np.array([af["danceability"] for af in audio_features])
+        idx = np.argsort(denceability)
+        N_third = int(len(audio_features)/3)
+        low_dance_idx = idx[0:N_third]
+        mid_dance_idx = idx[N_third: 2*N_third]
+        high_dance_idx = idx[N_third*2:]
+        sorted_idx = np.stack([mid_dance_idx, high_dance_idx, low_dance_idx[::-1]])
+        for idxs in sorted_idx:
+            sorted_songs.append(audio_features[idxs])
         
     return sorted_songs
